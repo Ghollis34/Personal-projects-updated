e6598662a08d4fd42357b36b0fa027d9f3f425f8
Ghollis34 <ghollis34@gmail.com>
2024-02-04 13:39:19 +0000

    changes

--- steamsticker.py
+++ steamsticker.py
@@ -32,24 +32,24 @@ number_of_stock_con = config.getint('number_bought', 'stock_con')
 number_of_ant_chall = config.getint('number_bought', 'ant_chall')
 
 
-
-
-stock_con = 5.32
+stock_con = 4.60
 stonks_stock_con = ((float(stock_con) - bought_stock_con)/bought_stock_con)*100
 
-stock_chall = 8.93
-stonks_stock_chall = ((float(stock_chall) - bought_stock_chall)/bought_stock_chall)*100
+stock_chall = 4.04
+stonks_stock_chall = (
+    (float(stock_chall) - bought_stock_chall)/bought_stock_chall)*100
 
-ant_chall = 1.00
+ant_chall = 0.64
 stonks_ant_chall = ((float(ant_chall) - bought_ant_chall)/bought_ant_chall)*100
 
-rio_leg = 0.55
+rio_leg = 0.29
 stonks_rio_leg = ((float(rio_leg) - bought_rio_leg)/bought_rio_leg)*100
 
-rio_chall = 0.63
+rio_chall = 0.38
 stonks_rio_chall = ((float(rio_chall) - bought_rio_chall)/bought_rio_chall)*100
 
-total_money_gain = ((stock_con*number_of_stock_con) + (stock_chall*number_of_stock_chall) + (ant_chall*number_of_ant_chall) + (rio_leg*number_of_rio_leg) + (rio_chall*number_of_rio_chall))
+total_money_gain = ((stock_con*number_of_stock_con) + (stock_chall*number_of_stock_chall) + (
+    ant_chall*number_of_ant_chall) + (rio_leg*number_of_rio_leg) + (rio_chall*number_of_rio_chall))
 total_percentage_gain = ((total_money_gain - 1000)/1000)*100
 
 if stock_con < 0:
@@ -57,21 +57,21 @@ if stock_con < 0:
 elif stock_con == 0:
     stock_con_colour = "black"
 else:
-    stock_con_colour = "green" 
+    stock_con_colour = "green"
 
 if stock_con < bought_stock_con:
     stock_con_label_colour = "red"
 elif stock_con == bought_stock_con:
     stock_con_label_colour = "black"
 else:
-    stock_con_label_colour = "green" 
+    stock_con_label_colour = "green"
 
 if stock_chall < 0:
     stock_chall_colour = "red"
 elif stock_chall == 0:
     stock_chall_colour = "black"
 else:
-    stock_chall_colour = "green" 
+    stock_chall_colour = "green"
 
 if stock_chall < bought_stock_chall:
     stock_chall_label_colour = "red"
@@ -106,7 +106,7 @@ if rio_leg < bought_rio_leg:
 elif rio_leg == bought_rio_leg:
     rio_leg_label_colour = "black"
 else:
-    rio_leg_label_colour = "green"#
+    rio_leg_label_colour = "green"
 
 if rio_chall < 0:
     rio_chall_colour = "red"
@@ -128,7 +128,7 @@ elif (total_money_gain-1000) == 0:
     total_money_colour = "black"
 else:
     total_money_colour = "green"
-    
+
 if total_percentage_gain < 0:
     total_percentage_colour = "red"
 elif total_percentage_gain == 0:
@@ -137,34 +137,58 @@ else:
     total_percentage_colour = "green"
 
 
-stock_con_label = Label(root, text="Current price of Stockholm Contenders: ").grid(row = 0, column = 0)
-stock_con_price_label = Label(root, text="£{:.2f}".format(stock_con), foreground=stock_con_label_colour, font=("", 10, "bold")).grid(row = 0, column = 1)
-stonks_stock_con_label = Label(root, text="Stockholm Contenders percentage gain = ").grid(row = 1, column = 0)
-stonks_stock_con_percentage_label = Label(root, text="{:.2f}".format(stonks_stock_con) + "%", foreground=stock_con_colour, font=("", 10, "bold")).grid(row = 1, column = 1)
+stock_con_label = Label(
+    root, text="Current price of Stockholm Contenders: ").grid(row=0, column=0)
+stock_con_price_label = Label(root, text="£{:.2f}".format(
+    stock_con), foreground=stock_con_label_colour, font=("", 10, "bold")).grid(row=0, column=1)
+stonks_stock_con_label = Label(
+    root, text="Stockholm Contenders percentage gain = ").grid(row=1, column=0)
+stonks_stock_con_percentage_label = Label(root, text="{:.2f}".format(
+    stonks_stock_con) + "%", foreground=stock_con_colour, font=("", 10, "bold")).grid(row=1, column=1)
 
-stock_chall_label = Label(root, text="Current price of Stockholm Challengers: ").grid(row = 2, column = 0)
-stock_chall_price_label = Label(root, text="£{:.2f}".format(stock_chall), foreground=stock_chall_label_colour, font=("", 10, "bold")).grid(row = 2, column = 1)
-stonks_stock_chall_label = Label(root, text="Stockholm Contenders percentage gain = ").grid(row = 3, column = 0)
-stonks_stock_chall_percentage_label = Label(root, text="{:.2f}".format(stonks_stock_chall) + "%", foreground=stock_chall_colour, font=("", 10, "bold")).grid(row = 3, column = 1)
+stock_chall_label = Label(
+    root, text="Current price of Stockholm Challengers: ").grid(row=2, column=0)
+stock_chall_price_label = Label(root, text="£{:.2f}".format(
+    stock_chall), foreground=stock_chall_label_colour, font=("", 10, "bold")).grid(row=2, column=1)
+stonks_stock_chall_label = Label(
+    root, text="Stockholm Contenders percentage gain = ").grid(row=3, column=0)
+stonks_stock_chall_percentage_label = Label(root, text="{:.2f}".format(
+    stonks_stock_chall) + "%", foreground=stock_chall_colour, font=("", 10, "bold")).grid(row=3, column=1)
 
-ant_chall_label = Label(root, text= "Current price of Antwerp Challengers: ").grid(row = 4, column = 0)
-ant_chall_price_label = Label(root, text="£{:.2f}".format(ant_chall), foreground=ant_chall_label_colour, font=("", 10, "bold")).grid(row = 4, column = 1)
-stonks_ant_chall_label = Label(root, text="Antwerp Challengers percentage gain = ").grid(row = 5, column = 0)
-stonks_ant_chall_percentage_label = Label(root, text="{:.2f}".format(stonks_ant_chall) + "%", foreground=ant_chall_colour, font=("", 10, "bold")).grid(row = 5, column = 1)
+ant_chall_label = Label(
+    root, text="Current price of Antwerp Challengers: ").grid(row=4, column=0)
+ant_chall_price_label = Label(root, text="£{:.2f}".format(
+    ant_chall), foreground=ant_chall_label_colour, font=("", 10, "bold")).grid(row=4, column=1)
+stonks_ant_chall_label = Label(
+    root, text="Antwerp Challengers percentage gain = ").grid(row=5, column=0)
+stonks_ant_chall_percentage_label = Label(root, text="{:.2f}".format(
+    stonks_ant_chall) + "%", foreground=ant_chall_colour, font=("", 10, "bold")).grid(row=5, column=1)
 
-rio_leg_label = Label(root, text="Current price of Rio Legends: ").grid(row = 6, column = 0)
-rio_leg_price_label = Label(root, text="£{:.2f}".format(rio_leg), foreground=rio_leg_label_colour, font=("", 10, "bold")).grid(row = 6, column = 1)
-stonks_rio_leg_label = Label(root, text="Rio Legends percentage gain = ").grid(row = 7, column = 0)
-stonks_rio_leg_percentage_label = Label(root, text="{:.2f}".format(stonks_rio_leg) + "%", foreground=rio_leg_colour, font=("", 10, "bold")).grid(row = 7, column = 1)
+rio_leg_label = Label(
+    root, text="Current price of Rio Legends: ").grid(row=6, column=0)
+rio_leg_price_label = Label(root, text="£{:.2f}".format(
+    rio_leg), foreground=rio_leg_label_colour, font=("", 10, "bold")).grid(row=6, column=1)
+stonks_rio_leg_label = Label(
+    root, text="Rio Legends percentage gain = ").grid(row=7, column=0)
+stonks_rio_leg_percentage_label = Label(root, text="{:.2f}".format(
+    stonks_rio_leg) + "%", foreground=rio_leg_colour, font=("", 10, "bold")).grid(row=7, column=1)
 
-rio_chall_label = Label(root, text="Current price of Rio Challengers: ").grid(row = 8, column = 0)
-rio_chall_price_label = Label(root, text="£{:.2f}".format(rio_chall), foreground=rio_chall_label_colour, font=("", 10, "bold")).grid(row = 8, column = 1)
-stonks_rio_chall_label = Label(root, text="Rio Challengers percentage gain = ").grid(row = 9, column = 0)
-stonks_rio_chall_percentage_label = Label(root, text="{:.2f}".format(stonks_rio_chall) + "%", foreground=rio_leg_colour, font=("", 10, "bold")).grid(row = 9, column = 1)
+rio_chall_label = Label(
+    root, text="Current price of Rio Challengers: ").grid(row=8, column=0)
+rio_chall_price_label = Label(root, text="£{:.2f}".format(
+    rio_chall), foreground=rio_chall_label_colour, font=("", 10, "bold")).grid(row=8, column=1)
+stonks_rio_chall_label = Label(
+    root, text="Rio Challengers percentage gain = ").grid(row=9, column=0)
+stonks_rio_chall_percentage_label = Label(root, text="{:.2f}".format(
+    stonks_rio_chall) + "%", foreground=rio_leg_colour, font=("", 10, "bold")).grid(row=9, column=1)
 
-total_money_gain_label = Label(root, text="Total money gain:").grid(row = 10, column = 0)
-total_money_gain_price_label = Label(root, text=" £{:.2f}".format(total_money_gain-1000), foreground=total_money_colour, font=("", 10, "bold")).grid(row = 10, column = 1)
-total_percentage_gain_label = Label(root, text="Total percentage gain:").grid(row = 11, column = 0)
-total_percentage_gain_price_label = Label(root, text=" {:.2f}".format(total_percentage_gain) + "%", foreground=total_percentage_colour, font=("", 10, "bold")).grid(row = 11, column = 1)
+total_money_gain_label = Label(
+    root, text="Total money gain:").grid(row=10, column=0)
+total_money_gain_price_label = Label(root, text=" £{:.2f}".format(
+    total_money_gain-1000), foreground=total_money_colour, font=("", 10, "bold")).grid(row=10, column=1)
+total_percentage_gain_label = Label(
+    root, text="Total percentage gain:").grid(row=11, column=0)
+total_percentage_gain_price_label = Label(root, text=" {:.2f}".format(
+    total_percentage_gain) + "%", foreground=total_percentage_colour, font=("", 10, "bold")).grid(row=11, column=1)
 
 root.mainloop()
